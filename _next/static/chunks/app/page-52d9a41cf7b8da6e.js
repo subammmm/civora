0064cba44df6260f7f2d432bb93f03b540fd0ab0
Shubham Dhakal <shubham.dhakal@trinity.edu.np>
2025-10-11 12:52:56 +0545

    Move static site files from out folder to root for GitHub Pages

---
+++ _next/static/chunks/app/page-52d9a41cf7b8da6e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931,404,327,301,951,635,875,385,943,905,955,201,854],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
